2aa6b0b0cdb450c442cb78dced5d8ab2678c43b7
Humberto Yupanqui Apaza <yupanquiah@gmail.com>
2026-02-09 16:56:32 -0500

    refactor: simplify SocialAuthButtons component and update button styles

--- src/auth/components/SocialAuthButtons.tsx
+++ src/auth/components/SocialAuthButtons.tsx
@@ -1,27 +1,19 @@
+"use client";
+
+import { Button } from "@heroui/react";
 import { IconBrandGithub, IconBrandGoogleFilled } from "@tabler/icons-react";
 
 import { authWithGitHub, authWithGoogle } from "@/auth/services/auth";
-import { Button } from "@/components/ui/button";
 
 export function SocialAuthButtons() {
   return (
-    <section className="flex flex-col space-y-3 space-x-3 md:flex-row">
+    <section className="flex flex-col gap-2">
       {" "}
-      <Button
-        size="lg"
-        variant="outline"
-        type="button"
-        onClick={authWithGoogle}
-      >
+      <Button variant="outline" onClick={authWithGoogle}>
         <IconBrandGoogleFilled />
         Iniciar sesión con Google
       </Button>
-      <Button
-        size="lg"
-        variant="outline"
-        type="button"
-        onClick={authWithGitHub}
-      >
+      <Button variant="outline" onClick={authWithGitHub}>
         <IconBrandGithub />
         Iniciar sesión con GitHub
       </Button>
