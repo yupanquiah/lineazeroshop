17509968ab344f62e113ac35adac81153d0d20c8
Humberto Yupanqui Apaza <yupanquiah@gmail.com>
2026-02-12 15:48:34 -0500

    refactor: update SocialAuthButtons layout and button styles for consistency

--- src/auth/components/SocialAuthButtons.tsx
+++ src/auth/components/SocialAuthButtons.tsx
@@ -1,22 +1,29 @@
-"use client";
-
-import { Button } from "@heroui/react";
 import { IconBrandGithub, IconBrandGoogleFilled } from "@tabler/icons-react";
 
 import { authWithGitHub, authWithGoogle } from "@/auth/services/auth";
+import { Button } from "@/components/ui/button";
 
 export function SocialAuthButtons() {
   return (
-    <section className="flex flex-col  gap-2">
-      {" "}
-      <Button className="w-full" variant="tertiary" onClick={authWithGoogle}>
+    <div className="inline-grid space-y-3">
+      <Button
+        className="cursor-pointer"
+        variant="outline"
+        type="button"
+        onClick={authWithGoogle}
+      >
         <IconBrandGoogleFilled />
         Iniciar sesión con Google
       </Button>
-      <Button className="w-full" variant="tertiary" onClick={authWithGitHub}>
+      <Button
+        className="cursor-pointer"
+        variant="outline"
+        type="button"
+        onClick={authWithGitHub}
+      >
         <IconBrandGithub />
         Iniciar sesión con GitHub
       </Button>
-    </section>
+    </div>
   );
 }
