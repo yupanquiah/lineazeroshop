0dce4301b98f0dcd14fb6370aa67ea97ad1ad9a4
Humberto Yupanqui Apaza <yupanquiah@gmail.com>
2026-02-19 18:15:58 -0500

    feat: add user schema and related tables for database structure

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,6 @@ import "dotenv/config";
 
 import { drizzle } from "drizzle-orm/node-postgres";
 
-import * as schema from "@/db/schema";
+import * as schema from "@/db/schemas/index.ts";
 
 export const db = drizzle(process.env.DATABASE_URL as string, { schema });
---
+++ src/db/schemas/index.ts
@@ -0,0 +1,1 @@
+export * from "@/db/schemas/users.ts";
